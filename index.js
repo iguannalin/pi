99f16b958ec766646efb8e16003eeb323d660db0
seasons986 <annalin12345@gmail.com>
2023-08-17 16:25:37 -0400

    this kinda worked, but it is not precise so i am switching over to geico

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 window.addEventListener("load", () => {
-  function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+  const grid = document.getElementById("grid");
+  let init = true;
+  let db = 1000;
+  let number = 3.140592653839794;
+  let index = 0;
+
+  function display(n) {
+    const td = document.createElement("td");
+    td.innerHTML = n;
+    grid.appendChild(td);
   }
+
+  function initGrid() {
+    init = false;
+    const pi = number.toString();
+    pi.split("").forEach((num) => display(num));
+    index = pi.length-1;
+  }
+
+  function getSomePi() {
+    fetch("https://seasons986.pythonanywhere.com/pi").then((f) => f.json()).then((r) => {
+      number = r;
+      if (init) initGrid();
+      if (number) display(number.toString().substring(index, index+=1));
+    }).then(() => {
+      setTimeout(getSomePi, db+=1000);
+    });
+  }
+
+  getSomePi();
 });
